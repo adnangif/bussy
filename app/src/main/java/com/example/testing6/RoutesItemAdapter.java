821a3c13f898b98d2e9216c1a5ef24262ab33e4c
adnangif <muhammadfahim010@gmail.com>
2023-01-09 00:09:44 +0600

    added RoutesItemAdapter.java

---
+++ app/src/main/java/com/example/testing6/RoutesItemAdapter.java
@@ -0,0 +1,16 @@
+package com.example.testing6;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RoutesItemAdapter {
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
